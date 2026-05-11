d6555a37872634baf782786dd2ee7afdadb57a1d
sunzhenbo <2407364332@.qq.com>
2026-05-11 14:41:03 +0800

    完整点评代码

--- src/main/java/com/hmdp/controller/VoucherOrderController.java
+++ src/main/java/com/hmdp/controller/VoucherOrderController.java
@@ -21,7 +21,6 @@ public class VoucherOrderController {
     @Autowired
     private IVoucherOrderService voucherOrderService;
 
-
     @PostMapping("seckill/{id}")
     public Result seckillVoucher(@PathVariable("id") Long voucherId) {
         return voucherOrderService.seckillVoucher(voucherId);
